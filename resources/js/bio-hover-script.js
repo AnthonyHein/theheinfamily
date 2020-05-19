a0fb8959821865dc96f95afc1d5d5f3649f7f433
Anthony Hein <anhein@princeton.edu>
2020-05-19 19:51:54 -0400

    Moving files over.

---
+++ resources/js/bio-hover-script.js
@@ -0,0 +1,23 @@
+function anthonysBio() {
+    $('#current-bio').html("Conversational, ask him about literally anything.");  
+}
+
+function dawnBio() {
+    $('#current-bio').html("Matriarch of the household, wields the power of the 'because-I-said-so'.");  
+}
+
+function anthonyBio() {
+    $('#current-bio').html("60% water, 20% brains, 20% Star Wars references, and 0% common sense.");  
+}
+
+function michaelBio() {
+    $('#current-bio').html("A fierce athlete and an even fiercer Samaritan. Get this kid on your team!");
+}
+
+function nicholasBio() {
+    $('#current-bio').html("The 'chaotic good' of the family, whatever that means . . .");
+}
+
+function noBio() {
+    $('#current-bio').html("Hover over an image to reveal a short bio about that squad member.");
+}
---
+++ resources/js/show-family-script.js
@@ -0,0 +1,15 @@
+function showFamily() {
+    console.log('Reached here.');
+    if ($('#show-more-family-icon').hasClass('fa-caret-down')) {
+        $('#show-more-family-icon').removeClass('fa-caret-down');
+        $('#show-more-family-icon').addClass('fa-caret-up');
+        $('#show-more-family-text').html('Show Less&nbsp;&nbsp;');
+        $('#extended-family-list').show();
+    }
+    else {
+        $('#show-more-family-icon').removeClass('fa-caret-up');
+        $('#show-more-family-icon').addClass('fa-caret-down');
+        $('#show-more-family-text').html('Show More&nbsp;&nbsp;');
+        $('#extended-family-list').hide();
+    }
+}
